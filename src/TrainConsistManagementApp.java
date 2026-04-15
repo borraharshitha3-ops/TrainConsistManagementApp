45b86c3033754c2f6b1c76449373df4cfcb35124
borraharshitha3-ops <borraharshitha3@gmail.com>
2026-04-15 11:30:08 +0530

    UC18: Linear search for bogie ID

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,23 +1,20 @@
-public static void bubbleSort(int[] capacities) {
-    int n = capacities.length;
+public static void linearSearch(String[] ids, String key) {
+    boolean found = false;
 
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (capacities[j] > capacities[j + 1]) {
-                int temp = capacities[j];
-                capacities[j] = capacities[j + 1];
-                capacities[j + 1] = temp;
-            }
+    for (String id : ids) {
+        if (id.equals(key)) {
+            found = true;
+            break;
         }
     }
 
-    System.out.println("Sorted Capacities:");
-    for (int c : capacities) {
-        System.out.print(c + " ");
-    }
+    if (found)
+        System.out.println("Bogie found: " + key);
+    else
+        System.out.println("Bogie not found");
 }
 
 void main() {
-    int[] capacities = {50, 30, 70, 20};
-    bubbleSort(capacities);
+    String[] ids = {"B1", "B2", "B3", "B4"};
+    linearSearch(ids, "B3");
 }
